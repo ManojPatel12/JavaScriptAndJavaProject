567fe76f1ccff13b7883a3af0edae13d4ea93e22
RahulPatel21 <91775723+RahulPatel21@users.noreply.github.com>
2021-10-02 12:31:48 +0530

    mvc RMI methods added for all case

--- complete/src/test/java/com/example/springboot/HelloControllerTest.java
+++ complete/src/test/java/com/example/springboot/HelloControllerTest.java
@@ -25,5 +25,11 @@ public class HelloControllerTest {
 		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
 				.andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+		mvc.perform(MockMvcRequestBuilders.get("/add").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+		mvc.perform(MockMvcRequestBuilders.get("/sub").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("Greetings from Spring Boot!")));
 	}
 }
