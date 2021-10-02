26d0b0dd61bbc26906c8345f47b56a220093d374
RahulPatel21 <91775723+RahulPatel21@users.noreply.github.com>
2021-10-02 12:45:16 +0530

    Number method for logs

--- initial/src/main/java/com/example/springboot/Application.java
+++ initial/src/main/java/com/example/springboot/Application.java
@@ -15,9 +15,9 @@ public class Application {
 		System.out.println("Let's inspect the beans provided by Spring Boot:");
 
 		String[] beanNames = ctx.getBeanDefinitionNames();
-		Arrays.sort(beanNames);
-		for (String beanName : beanNames) {
-			System.out.println(beanName);
+		
+		for (int beanNumber : beanNumbers) {
+			System.out.println(beanNumbers);
 		}
 	}
 	
